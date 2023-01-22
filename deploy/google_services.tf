fa8e7e67a3c0db108ec79913e49b1822597feeb8
Ian Brown <email@ian-b.com>
2023-01-22 15:03:00 +0000

    Add Cloud Resource Manager API's

--- deploy/google_services.tf
+++ deploy/google_services.tf
@@ -7,3 +7,8 @@ resource "google_project_service" "service_firebase" {
   project = local.project_id
   service = "firebase.googleapis.com"
 }
+
+resource "google_project_service" "service_cloud_resource_manager" {
+  project = local.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
